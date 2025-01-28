3e5df30365c6998d479339232561aa57e9b955a2
Abhiram K Rajan <abhirammain01@gmail.com>
2025-01-28 11:13:05 +0530

    Initial commit

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function Cards({ data }) {
+  return (
+    
+      <Card className='h-100 cards'>
+        <Card.Img variant="top" src={data.image} className='productimage' />
+        <Card.Body>
+          <Card.Title>{data.title.slice(0,30)}</Card.Title>
+          <Card.Text>
+           {data.description.slice(0,100)}...
+          </Card.Text>
+          <div className="d-flex justify-content-between align-items-center">
+          {/* d-flex justify-content-between align-items-center */}
+          <p className='mb-0 fw-bold'>${data.price}</p>
+          
+          <Button variant="primary" className='p-1 rounded-0'>View Details</Button>
+          </div>
+          
+        </Card.Body>
+      </Card>
+    
+  )
+}
+
+export default Cards
